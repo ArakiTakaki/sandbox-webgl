d6707d9424eb9b7cd319ee69776c1f64b611ad10
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-06-17 17:04:24 +0900

    ベースとなるクラスの定義

---
+++ src/shader/fragmentConstants.ts
@@ -0,0 +1,6 @@
+export default {
+  INV_MATRIX: 'invMatrix',
+  LIGHT_POSITION: 'lightPosition',
+  EYE_DIRECTION: 'eyeDirection',
+  AMBIENT_COLOR: 'ambientColor',
+} as const;
---
+++ src/shader/vertexConstants.ts
@@ -0,0 +1,7 @@
+export default {
+  POSITION: 'position',
+  NORMAL: 'normal',
+  COLOR: 'color',
+  MVP_MATRIX: 'mvpMatrix',
+  M_MATRIX: 'mMatrix',
+} as const;
